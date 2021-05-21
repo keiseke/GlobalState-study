20714b5ec343fdac007bc4ad83d01c77269974e5
keiseke <81750996+keiseke@users.noreply.github.com>
2021-05-21 10:58:13 +0900

    GlobalState-study_finished

--- src/components/molecules/UserIconWithName.jsx
+++ src/components/molecules/UserIconWithName.jsx
@@ -1,14 +1,26 @@
+import React, { memo, useContext } from "react";
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { UserContext } from "../../providers/UserProvider";
+import { userState } from "../../store/userState";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
+  console.log("UserIconWithName");
+
   const { image, name } = props;
+  // const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+  console.log(userInfo);
+
   return (
     <SContainer>
       <SImage height={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
@@ -22,4 +34,9 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
